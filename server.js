dba2730209368a9b309da5fee71e117df0760630
Glitch (timsongcoder-boilerplate-project-urlshortener) <none>
2018-12-06 02:33:15 +0000

    🍅😚 Checkpoint ./server.js:2374645/1064

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose');
 const dns = require('dns');
 const bodyParser = require('body-parser');
 const urlValidator = require('valid-url');
-// const autoIncrementor = require('mongoose-auto-increment');
+const autoIncrementor = require('mongoose-auto-increment');
 
 var cors = require('cors');
 
@@ -16,11 +16,11 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-mongoose.connect(process.env.MONGO_URI);
-// autoIncrementor.initialize(dbConnection);
+const dbConnection = mongoose.connect(process.env.MONGO_URI);
+autoIncrementor.initialize(dbConnection);
 const mappingSchema = new mongoose.Schema({original_url: {type: String, required: true}, short_url: {type: Number, required: true}});
-// mappingSchema.plugin(autoIncrementor.plugin, {model: 'Mapping', field: 'short_url', startAt: 1});
-const Mapping = mongoose.model('Mapping', mappingSchema);
+mappingSchema.plugin(autoIncrementor.plugin, {model: 'Mapping', field: 'short_url', startAt: 1});
+const Mapping = dbConnection.model('Mapping', mappingSchema);
 app.use(cors());
 
 
@@ -48,8 +48,12 @@ app.post('/api/shorturl/new', (req, res) => {
   const original_url = req.body.url;
   console.log(original_url);
   if(urlValidator.isUri(original_url)) {
-    // TODO: Ideally, we need an auto-incremented, unique number, not a random one
-    const count = Math.floor(Math.random() * 1000000);
+    Mapping.nextCount((err, count) => {
+      if(err){
+        console.log('Can not get next count.');
+        res.json({error: 'Internal DB Error'});
+        return;
+      }
       console.log(`count: ${count}`);
       Mapping.create({original_url, short_url: count}, (err, doc) => {
         if(err) {
@@ -59,8 +63,7 @@ app.post('/api/shorturl/new', (req, res) => {
           res.json({original_url: doc.original_url, short_url: doc.short_url});
         }
       });
-      
-    ;
+    });
   } else {
     res.json({error: 'invalid URL'});
   }
